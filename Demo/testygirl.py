6b883290e6951829386fd6b852d5237576c2750a
Braxton Walters <braxton.walters@gmail.com>
2023-07-03 13:58:53 -0400

    removed tracking for demo

--- Demo/testygirl.py
+++
@@ -1,9 +0,0 @@
-class Book:
-    all_books = []
-    
-    def __init__(self, name, year, num_of_pages):
-        self.name = name
-        self.year = year
-        self.num_of_pages = num_of_pages
-
-        Book.all_books.append(self)
