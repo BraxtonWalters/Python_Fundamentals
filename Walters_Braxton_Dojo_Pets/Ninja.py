2ef0ad63aca28d9c6f1b2fb7af4febd81b8de493
Braxton Walters <braxton.walters@gmail.com>
2023-07-04 17:57:24 -0400

    finished dojo pets asgmt

--- Walters_Braxton_Dojo_Pets/Ninja.py
+++ Walters_Braxton_Dojo_Pets/Ninja.py
@@ -1,22 +1,35 @@
+import Pets
+
 class Ninja:
 
     # implement __init__( first_name , last_name , treats , pet_food , pet )
-    def __init__(self, first_name, last_name, treats, pet_food, pet):
+    def __init__(self, first_name, last_name, treats, pet_food, pet_name, pet_type, pet_tricks ):
         self.first_name = first_name
         self.last_name = last_name
         self.treats = treats
         self.pet_food = pet_food
-        self.pet = pet
+        self.pet = Pets.Dog(pet_name, pet_type, pet_tricks)
 
     # implement the following methods:
     # walk() - walks the ninja's pet invoking the pet play() method
     def walk(self):
-        pass
+        self.pet.play()
+        return self
 
     # feed() - feeds the ninja's pet invoking the pet eat() method
     def feed(self):
-        pass
+        self.pet.eat()
+        return self
 
     # bathe() - cleans the ninja's pet invoking the pet noise() method
     def bathe(self):
-        pass
+        self.pet.noise()
+        return self
+
+    def show_instance(self):
+        print(self.first_name)
+        print(self.last_name)
+        print(self.treats)
+        print(self.pet_food)
+        print(self.pet.name)
+        return self
--- Walters_Braxton_Dojo_Pets/Pets.py
+++ Walters_Braxton_Dojo_Pets/Pets.py
@@ -5,20 +5,34 @@ class Pet:
         self.name = name
         self.type = type
         self.tricks = tricks
+        self.health = 50
+        self.energy = 10
 
     # implement the following methods:
     # sleep() - increases the pets energy by 25
     def sleep(self):
-        pass
+        self.health += 25
 
     # eat() - increases the pet's energy by 5 & health by 10
     def eat(self):
-        pass
+        self.energy += 5
+        self.health += 10
     
     # play() - increases the pet's health by 5
     def play(self):
-        pass
+        self.health += 5
 
     # noise() - prints out the pet's sound
     def noise(self):
-        pass
+        print("woof woof")
+
+    def show_pet_instance(self):
+        print(self.name)
+        print(self.type)
+        print(self.health)
+        print(self.energy)
+        return self
+
+class Dog(Pet):
+    def __init__(self, name, type, tricks):
+        super().__init__(name, type, tricks)
