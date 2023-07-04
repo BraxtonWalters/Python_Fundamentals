986f62bebba6a9fa8137d680e24fc55b917010ca
Braxton Walters <braxton.walters@gmail.com>
2023-07-04 14:34:36 -0400

    added files for dojo pets

---
+++ Walters_Braxton_Dojo_Pets/Pets.py
@@ -0,0 +1,24 @@
+class Pet:
+
+    # implement __init__( name , type , tricks ):
+    def __init__(self, name, type, tricks):
+        self.name = name
+        self.type = type
+        self.tricks = tricks
+
+    # implement the following methods:
+    # sleep() - increases the pets energy by 25
+    def sleep(self):
+        pass
+
+    # eat() - increases the pet's energy by 5 & health by 10
+    def eat(self):
+        pass
+    
+    # play() - increases the pet's health by 5
+    def play(self):
+        pass
+
+    # noise() - prints out the pet's sound
+    def noise(self):
+        pass
